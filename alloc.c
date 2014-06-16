01ba4371dd0d4afa78e9005abd4ac5f2dae808fc
Paul Bone <paul@bone.id.au>
2014-06-16 16:58:27 +1000

    Add callbacks that can be used by a profiler
    Add callback functions that can be used by a profiler, such as Mercury's
    parallel execution profiler (formerly called ThreadScope support).
    
    ainclude/gc.h:
        Declare all the callbacks
    
    alloc.c:
        Use callbacks for when GC starts and stops.
    
    pthread_stop_world.c:
        Use callbacks for when a thread is stopped for garbage collection.
    
    This code was originaly developed by myself on 3/12/2009.

--- alloc.c
+++ alloc.c
@@ -86,6 +86,11 @@ word GC_gc_no = 0;
   }
 #endif /* !NO_CLOCK */
 
+void (*GC_mercury_callback_start_collect)(void) = NULL;
+void (*GC_mercury_callback_stop_collect)(void) = NULL;
+void (*GC_mercury_callback_pause_thread)(void) = NULL;
+void (*GC_mercury_callback_resume_thread)(void) = NULL;
+
 #ifndef GC_DISABLE_INCREMENTAL
   GC_INNER GC_bool GC_incremental = FALSE; /* By default, stop the world. */
   STATIC GC_bool GC_should_start_incremental_collection = FALSE;
@@ -621,6 +626,9 @@ GC_INNER GC_bool GC_try_to_collect_inner(GC_stop_func stop_func)
     GC_clear_marks();
     SAVE_CALLERS_TO_LAST_STACK();
     GC_is_full_gc = TRUE;
+    if (GC_mercury_callback_start_collect) {
+      GC_mercury_callback_start_collect();
+    }
     if (!GC_stopped_mark(stop_func)) {
       if (!GC_incremental) {
         /* We're partially done and have no way to complete or use      */
@@ -656,6 +664,9 @@ GC_INNER GC_bool GC_try_to_collect_inner(GC_stop_func stop_func)
                         time_diff, ns_frac_diff);
       }
 #   endif
+    if (GC_mercury_callback_stop_collect) {
+      GC_mercury_callback_stop_collect();
+    }
     if (GC_on_collection_event)
       GC_on_collection_event(GC_EVENT_END);
     return(TRUE);
--- include/gc.h
+++ include/gc.h
@@ -412,6 +412,27 @@ struct GC_timeval_s {
   unsigned long tv_nsec;/* nanoseconds fraction (<1000000) */
 };
 
+/*
+ * Callbacks for mercury to notify the runtime of certain events.
+ */
+GC_API void (*GC_mercury_callback_start_collect)(void);
+                /* Starting a collection */
+GC_API void (*GC_mercury_callback_stop_collect)(void);
+                /* Stopping a collection */
+GC_API void (*GC_mercury_callback_pause_thread)(void);
+                /*
+                 * This thread is about to be paused.
+                 *
+                 * Use these with care!  They're called from a signal handler,
+                 * they must NOT allocate memory and if they do locking they
+                 * must use reentrant mutexes.  Also note that these do not
+                 * work on OS X/Darwin.  On Darwin the world is stopped in a
+                 * different way, we can't easily add support for these
+                 * callbacks on Darwin. 
+                 */
+GC_API void (*GC_mercury_callback_resume_thread)(void);
+                /* This thread is about to be resumed */
+
 /* Public procedures */
 
 /* Set/get the time limit of the incremental collections.  This is      */
